9a3589421d387d956baa366c01b243ad043022a6
916RasnitaRadu <91827415+916RasnitaRadu@users.noreply.github.com>
2022-07-13 18:57:41 +0300

    Add files via upload

---
+++ First Year/Semester 2/Graph Algo/PW5/src/main.py
@@ -0,0 +1,7 @@
+from PracticWork5.src.UI.ui import *
+
+if __name__ == '__main__':
+    file_path = "D:\Faculta\GraphAlgo\Practical_Work no.5\PracticWork5\src\graph_test"
+    ui = UI(file_path)
+    ui.run_console()
+
