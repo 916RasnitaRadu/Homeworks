a9dab332600635f86a4f1c5ee31396f28af878f7
916RasnitaRadu <91827415+916RasnitaRadu@users.noreply.github.com>
2022-07-13 18:51:22 +0300

    Add files via upload

---
+++ First Year/Semester 2/Graph Algo/PW3/src/main.py
@@ -0,0 +1,8 @@
+from PracticWork3.src.UI.ui import *
+
+
+if __name__ == '__main__':
+    file_path = "D:\Faculta\GraphAlgo\Practical_Work no.3\PracticWork3\src\graph10k.txt"
+    ui = UI(file_path)
+    ui.run_console()
+
