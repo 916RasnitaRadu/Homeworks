5573ab7af65ed5424d7faa0e6538996c908e9fd4
916RasnitaRadu <91827415+916RasnitaRadu@users.noreply.github.com>
2022-07-13 18:54:33 +0300

    Add files via upload

---
+++ First Year/Semester 2/Graph Algo/PW4/src/main.py
@@ -0,0 +1,7 @@
+from PracticWork4.src.UI.ui import *
+
+
+if __name__ == '__main__':
+    file_path = "D:\Faculta\GraphAlgo\Practical_Work no.4\8_and_12.txt"
+    ui = UI(file_path)
+    ui.run_console()
